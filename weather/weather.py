8e70d5e2dda71fdb0b3c00ac28baeca8fda589fc
93design <goegebeurjamie@gmail.com>
2018-11-20 15:52:26 +0000

    Adding weatert to the repo

---
+++ weather/weather.py
@@ -0,0 +1,24 @@
+from requests import get
+import matplotlib.pyplot as plt
+from dateutil import parser
+
+url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallmeasurements/505307'
+weather = get(url).json()
+
+## list comprehension to get all the temperatures in a list
+temperatures = [record['ambient_temp'] for record in weather['items']]
+
+
+## list comprehension to get all the temperatures in a list in a readable format
+timestamps = [parser.parse(record['reading_timestamp']) for record in weather['items']]
+
+
+## create a plot of timestamps against temperature and show it
+plt.plot(timestamps, temperatures)
+plt.show()
+
+plt.plot(timestamps, temperatures)
+## Set the axis labels
+plt.ylabel('Temperature')
+plt.xlabel('Time')
+plt.show()
